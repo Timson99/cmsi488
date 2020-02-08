b680ad8f3447793cf4a6c6b2271821fe1a173721
Tim Herrmann <tvherrmann@comcast.net>
2020-02-08 11:44:36 -0800

    Added Comments to 1 and 2 Header

--- homework1/new_ael_interpreter.js
+++ homework1/new_ael_interpreter.js
@@ -1,17 +1,27 @@
 // An extended interpreter for Ael.
+//
+// Extensions Include:
+//  1) A Exponentiation Operator, **
+//        Associativity: Right Associative (2**2**3 = 2**8 = 256)
+//        Negative Exponent Rules: To avoid ambiguity, no negative exponents are allowed.
+//  2) A while loop, while() {}
+//        Loop Condition: Takes in a number or id, breaks loop if 0.
+//        Loop Block: A loop block must have at least one statement followed
+//                    by a semicolon (since statement-less while loops run infinitely).
+//
 // Example usage:
 //   node new_ael_interpreter.js "print 2; x = 22; print 1 + x / 2;"
 //   2
 //   12
 //
-//   node new_ael_interpreter.js "x = 2 ** 2 ** 3; while x { x = x - 1; print x;};"
-//   255
-//   254
-//   ...
+//   node new_ael_interpreter.js "x = 2 ** 2; while x { x = x - 1; print x;};"
+//   3
+//   2
+//   1
 //   0
 //
-//   node new_ael_interpreter.js "print 2 * 2 ** -3;"
-//   0.25
+//   node new_ael_interpreter.js "print 2 ** 2 ** 3;"
+//   256
 
 const ohm = require('ohm-js');
 
@@ -30,8 +40,8 @@ const aelGrammar = ohm.grammar(`Ael {
   Factor    = "-" Primary                     --negate
             | Power
   Power     = Primary "**" Power              --raise
-            | "-" Primary                     --negate
             | Primary
+
   Primary   = "(" Exp ")"                     --parens
             | number
             | id
@@ -48,14 +58,13 @@ const semantics = aelGrammar.createSemantics().addOperation('exec', {
   Block(_open, ss, _semicolons, _close) { ss.exec(); },
   Statement_assign(i, _eq, e) { memory.set(i.sourceString, e.eval()); },
   Statement_print(_, e) { console.log(e.eval()); },
-  Statement_while(_, id_num, block) { while(id_num.eval()) { block.exec(); }},
+  Statement_while(_, id_num, block) { while (id_num.eval()) { block.exec(); } },
 }).addOperation('eval', {
-Exp_plus(e, _op, t) { return e.eval() + t.eval(); },
+  Exp_plus(e, _op, t) { return e.eval() + t.eval(); },
   Exp_minus(e, _op, t) { return e.eval() - t.eval(); },
   Term_times(t, _op, f) { return t.eval() * f.eval(); },
   Term_divide(t, _op, f) { return t.eval() / f.eval(); },
   Factor_negate(_op, p) { return -p.eval(); },
-  Power_negate(_op, p) { return -p.eval(); },
   Power_raise(p, _op, pow) { return p.eval() ** pow.eval(); },
   Primary_parens(_open, e, _close) { return e.eval(); },
   number(_chars) { return +this.sourceString; },
